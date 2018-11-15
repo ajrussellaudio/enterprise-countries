1e6360a9323c345cd5b732fc64aaf1caedd3075d
Alan Russell <ajrussellaudio@gmail.com>
2018-11-15 10:27:21 +0000

    add languages selector

--- src/store/countries/selectors.spec.ts
+++ src/store/countries/selectors.spec.ts
@@ -1,4 +1,4 @@
-import { getAllCountries } from "./selectors";
+import { getAllCountries, getLanguages } from "./selectors";
 import { dummyCountries } from "./testHelpers";
 import { ApplicationState } from "..";
 
@@ -9,5 +9,29 @@ describe("getAllCountries", () => {
       filter: {}
     };
     expect(getAllCountries(dummyState)).toEqual(dummyCountries);
+  });
+});
+
+describe("getAllLanguages", () => {
+  it("gets all the languages, no duplicates", () => {
+    const dummyState: ApplicationState = {
+      countries: { list: dummyCountries, isLoading: false },
+      filter: {}
+    };
+    const expectedLanguages = [
+      {
+        iso639_1: "en",
+        iso639_2: "eng",
+        name: "English",
+        nativeName: "English"
+      },
+      {
+        iso639_1: "wk",
+        iso639_2: "wkn",
+        name: "Wakandan",
+        nativeName: "Wakando"
+      }
+    ];
+    expect(getLanguages(dummyState)).toEqual(expectedLanguages);
   });
 });
--- src/store/countries/selectors.ts
+++ src/store/countries/selectors.ts
@@ -1,4 +1,23 @@
 import { ApplicationState } from "..";
+import { createSelector } from "reselect";
+import { uniqBy } from "lodash";
+import { Country } from "./types";
+import { Language } from "../filter/types";
 
 export const getAllCountries = (state: ApplicationState) =>
   state.countries.list;
+
+export const getLanguages = createSelector(
+  getAllCountries,
+  (countries: Country[]) =>
+    uniqBy(
+      countries.reduce(
+        (languages: Language[], country: Country) => [
+          ...languages,
+          ...country.languages
+        ],
+        []
+      ),
+      "iso639_1"
+    )
+);
